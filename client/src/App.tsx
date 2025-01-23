ec985c5bce38ceed68a1d79e9e6002d9f8343d4f
Jonas Andersen <Jonas@anders1.com>
2025-01-23 21:24:47 +0100

    UPT: MP3 Sounds

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
+import blockDropSound from "./assets/sounds/block-drop.mp3";
+
 function App() {
+  const playSound = () => {
+    const audio = new Audio(blockDropSound);
+    audio.play();
+  };
+
   return (
     <div>
       <h1>Hello World</h1>
+      <button onClick={playSound}>Play Sound</button>
     </div>
   );
 }
